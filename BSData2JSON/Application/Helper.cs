75fbc115f3f66aad17a4c47f0daf9a1e3d9fc0cb
Mayhem50 <Mayhem50@gmail.com>
2024-01-12 19:42:06 +1100

    First check-in

---
+++ BSData2JSON/Application/Helper.cs
@@ -0,0 +1,122 @@
+﻿namespace BSData2JSON.Application
+{
+    using BSData2JSON.Models;
+    using System.Xml;
+
+    public class BSDataUtil
+    {
+        private readonly XmlNamespaceManager _nsmgr;
+
+        private Unit rtnUnit;
+
+        public BSDataUtil(XmlNamespaceManager m)
+        {
+            _nsmgr = m;
+        }
+
+        public Unit GetBaseLineDetails(XmlNode unitNode, string unitId, string unitName)
+        {
+            rtnUnit = new Unit() { UnitName = unitName };
+
+            var baseStatNodes = unitNode.SelectNodes(string.Format("//{0}:categoryLink[@targetId = '{1}']/ancestor::{0}:selectionEntry//{0}:profile[@typeId = 'c547-1836-d8a-ff4f']//{0}:characteristic", "BSD", unitId), _nsmgr);
+
+            var m = baseStatNodes.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "M")
+                .Select(x => x.InnerText).FirstOrDefault();
+
+            var t = baseStatNodes.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "T")
+                .Select(x => x.InnerText).FirstOrDefault();
+
+            var sv = baseStatNodes.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "SV")
+                .Select(x => x.InnerText).FirstOrDefault();
+
+            var w = baseStatNodes.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "W")
+                .Select(x => x.InnerText).FirstOrDefault();
+
+            var ld = baseStatNodes.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "LD")
+                .Select(x => x.InnerText).FirstOrDefault();
+
+            var oc = baseStatNodes.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "LD")
+                .Select(x => x.InnerText).FirstOrDefault();
+
+            rtnUnit.Movement = m;
+            rtnUnit.Toughness = t;
+            rtnUnit.Save = sv;
+            rtnUnit.Wounds = w;
+            rtnUnit.Leadership = ld;
+            rtnUnit.ObjectControl = oc;
+
+            var InvulnerableSave = unitNode.SelectNodes(string.Format("//{0}:categoryLink[@targetId = '{1}']/ancestor::{0}:selectionEntry//{0}:profile[@typeId = '9cc3-6d83-4dd3-9b64' and @name='Invulnerable Save']//{0}:characteristic", "BSD", unitId), _nsmgr);
+            if (InvulnerableSave.Count == 1)
+            {
+                rtnUnit.Invulnerability = InvulnerableSave[0].InnerText;
+            }
+
+            var abilities = unitNode.SelectNodes(string.Format("//{0}:categoryLink[@targetId = '{1}']/ancestor::{0}:selectionEntry//{0}:profile[@typeId = '9cc3-6d83-4dd3-9b64' and @name!='Invulnerable Save']", "BSD", unitId), _nsmgr);
+
+            foreach (XmlNode n in abilities)
+            {
+                string abilityName = n.Attributes["name"].Value;
+                string abilityId = n.Attributes["id"].Value;
+
+                var AbilityTextNode = unitNode.SelectNodes(string.Format("//{0}:profile[@typeId = '9cc3-6d83-4dd3-9b64' and @id='{1}']", "BSD", abilityId), _nsmgr);
+
+                rtnUnit.Abilities.Add(new Ability()
+                {
+                    AbilityName = abilityName,
+                    AbilityText = AbilityTextNode[0].InnerText
+                });
+            }
+
+            var RangedWeapons = unitNode.SelectNodes(string.Format("//{0}:categoryLink[@targetId = '{1}']/ancestor::{0}:selectionEntry//{0}:profile[@typeName = 'Ranged Weapons']/../..", "BSD", unitId), _nsmgr);
+            Console.WriteLine($"Number of ranged weapons found: {RangedWeapons.Count}");
+
+            foreach (XmlNode rw in RangedWeapons)
+            {
+                string weaponName = rw.Attributes["name"].Value;
+                string skillId = rw.Attributes["id"].Value;
+
+
+                var weaponProfiles = unitNode.SelectNodes(string.Format("//{0}:selectionEntry[@id = '{1}']//{0}:profile[@typeName = 'Ranged Weapons']", "BSD", skillId), _nsmgr);
+                foreach (var item in weaponProfiles) // each weapon profile.
+                {
+                    var a = "";
+
+
+                    var u = new Unit()
+                    {
+
+
+                    };
+
+                }
+
+
+
+
+
+                // var attacks = rw.Cast<XmlNode>().Where(x => x.Attributes["name"].Value == "A")
+                //.Select(x => x.InnerText).FirstOrDefault();
+
+
+            }
+
+            var MeleedWeapons = unitNode.SelectNodes(string.Format("//{0}:categoryLink[@targetId = '{1}']/ancestor::{0}:selectionEntry//{0}:profile[@typeName = 'Melee Weapons']/../..", "BSD", unitId), _nsmgr);
+            Console.WriteLine($"Number of melee weapons found: {MeleedWeapons.Count}");
+
+            foreach (XmlNode rw in MeleedWeapons)
+            {
+                string weaponName = rw.Attributes["name"].Value;
+                string skillId = rw.Attributes["id"].Value;
+
+                var AbilityTextNode = unitNode.SelectNodes(string.Format("//{0}:profile[@typeId = '9cc3-6d83-4dd3-9b64' and @id='{1}']", "BSD", skillId), _nsmgr);
+
+                rtnUnit.MeleeSkills.Add(new MeleeSkill()
+                {
+                    WeaponName = weaponName,
+                });
+            }
+
+            return rtnUnit;
+        }
+    }
+}
---
+++ BSData2JSON/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace BSData2JSON
+{
+    using BSData2JSON.Application;
+    using BSData2JSON.Models;
+    using Newtonsoft.Json;
+    using System.IO;
+    using System.Xml;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("BSData2JSON Battlescribe Data 2 JSON");
+            var CWD = Environment.CurrentDirectory;
+            Console.WriteLine($"Current working directory:{CWD}");
+
+            DirectoryInfo inDir = new DirectoryInfo(CWD + "/in");
+            if (!inDir.Exists) { inDir.Create(); }
+
+            DirectoryInfo outDir = new DirectoryInfo(CWD + "/out");
+            if (!outDir.Exists) { outDir.Create(); }
+
+            var catFIles = inDir.GetFiles("*.cat");
+
+            if (catFIles.Length == 0)
+            {
+                Console.WriteLine("No files to process. Please ensure you place the data failes into in directory with this application.");
+            }
+
+            XmlDocument doc = new XmlDocument();
+            foreach (FileInfo catfile in catFIles)
+            {
+                Console.WriteLine($"Processing{catfile.Name}");
+
+                doc.Load(catfile.FullName);
+
+                var nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("BSD", "http://www.battlescribe.net/schema/catalogueSchema");
+
+                BSDataUtil dataUtil = new BSDataUtil(nsmgr);
+
+                var nodes = doc.SelectNodes("//BSD:categoryEntries/BSD:categoryEntry", nsmgr);
+
+                //var nodes = doc.SelectNodes("//*[local-name()='categoryEntry']");
+
+                foreach (XmlNode node in nodes)
+                {
+                    var unitName = node.Attributes["name"].Value.ToString();
+                    Console.WriteLine(unitName);
+
+                    var unitId = node.Attributes["id"].Value.ToString();
+                    Console.WriteLine(unitId);
+
+                    var cNode = doc.SelectNodes(string.Format("//{0}:categoryLink[@targetId = '{1}']/ancestor::{0}:selectionEntry", "BSD", unitId), nsmgr);
+                    XmlNode UnitXmlNode = cNode[0];
+
+                    if (UnitXmlNode != null)
+                    {
+                        Unit u = dataUtil.GetBaseLineDetails(UnitXmlNode, unitId, unitName);
+                        string unitString = JsonConvert.SerializeObject(u, Newtonsoft.Json.Formatting.Indented);
+
+                        var writeFile = new FileInfo(outDir + "/" + catfile.Name.Replace(".cat","") + "/" + unitName + ".txt");
+                        if (!writeFile.Directory.Exists)
+                                writeFile.Directory.Create();
+
+
+                        using (StreamWriter writetext = new StreamWriter(writeFile.FullName))
+                        {
+                            writetext.WriteLine(unitString);
+                        }
+                    }
+
+                    //Console.WriteLine(x);
+
+                    //var unitXMLDoc = new XmlDocument();
+                    //unitXMLDoc.LoadXml(cNode[0].InnerXml);
+
+                    //var stateLineNode =
+                    //.SelectNodes(string.Format("//{0}:profiles", "BSD", unitId), nsmgr);
+                }
+            }
+
+            Console.WriteLine("Application has completed, press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
